9f7b5dcfd3d89113058f9b5acc6b8d8414cc92a8
Aimé Mathieu <maime@excilys.com>
2017-04-11 18:09:34 +0200

    Ajout exception

---
+++ src/exceptions/ComputerExistException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class ComputerExistException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7221680426615401995L;
+
+	public ComputerExistException() {
+		super("Computer Already Exist");
+	}
+
+}
---
+++ src/exceptions/ComputerNotFoundException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class ComputerNotFoundException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 4596262789695026102L;
+
+	public ComputerNotFoundException() {
+		super("Computer Not Foud");
+	}
+}
