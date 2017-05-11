1a510bfee11046746d250b87f1af8fdef3ab2b34
Mathieu Aimé <maime@excilys.com>
2017-05-11 15:30:47 +0200

    [Feature] custom configuration for mysql container

--- docker/MySQL/sqlScript/createComputerSql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "SET GLOBAL max_allowed_packet=83886080; INSERT IGNORE INTO computer (id,name,company_id) VALUES " > insertComputer.sql
-for i in {1..1000000}
-do
-   echo "($i,CONCAT('test',$i),1)," >> insertComputer.sql
-done
