e3224f7fdc6215550faaae310ba95195a206de84
Mathieu Aimé <maime@excilys.com>
2017-05-22 15:23:20 +0200

    [Refactor] Gitignore module

--- modules-root/persistence/target/classes/config/db/2-PRIVILEGES.sql
+++
@@ -1,9 +0,0 @@
-  #-----------------------------------
-  #USER RIGHTS MANAGEMENT
-  #-----------------------------------
-  CREATE USER 'admincdb'@'localhost' IDENTIFIED BY 'qwerty1234';
-
-  GRANT ALL PRIVILEGES ON `computer-database-db-gatling`.* TO 'admincdb'@'localhost' WITH GRANT OPTION;
-
-
-  FLUSH PRIVILEGES;
