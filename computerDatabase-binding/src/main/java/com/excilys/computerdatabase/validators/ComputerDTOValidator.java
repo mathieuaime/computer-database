bc8117cdcd96feffdc74bd516bb3744dbf85fd6c
Mathieu Aimé <maime@excilys.com>
2017-06-07 17:21:48 +0200

    [Fix] validator

--- computerDatabase-binding/src/main/java/com/excilys/computerdatabase/validators/ComputerDTOValidator.java
+++ computerDatabase-binding/src/main/java/com/excilys/computerdatabase/validators/ComputerDTOValidator.java
@@ -18,7 +18,7 @@ import com.excilys.computerdatabase.dtos.ComputerDTO;
 
 public class ComputerDTOValidator implements ConstraintValidator<VerificationComputerDTO, ComputerDTO> {
     private static final Logger LOGGER = LoggerFactory.getLogger(ComputerDTOValidator.class);
-    private static final String DATE_FRANCAISE = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})";
+    private static final String DATE_FRANCAISE = "^(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-[0-9]{4}";
     private static final String DATE_FRANCAISE_ENVERS = "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])";
     private static final String DATE_ANGLAISE = "^(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])-[0-9]{4}";
 
--- computerDatabase-binding/src/test/java/com/excilys/computerdatabase/validators/ComputerDTOValidatorTest.java
+++ computerDatabase-binding/src/test/java/com/excilys/computerdatabase/validators/ComputerDTOValidatorTest.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.*;
 
 import java.time.LocalDate;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import com.excilys.computerdatabase.dtos.ComputerDTO;
+
 
 public class ComputerDTOValidatorTest {
 
@@ -65,4 +68,31 @@ public class ComputerDTOValidatorTest {
         assertEquals(false,ComputerDTOValidator.verificationName(faux3));
     }
 
+    @Test
+    public void testRegex () {
+        String date = "01/02/2000";
+        String date1 = "01-02-2000";
+        String date2 = "1999-01-01";
+        String date3 = "12-20-1999";
+        String regexDateFrancaise ="^(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-[0-9]{4}";
+        String regexDateFrancaise2 ="^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])";
+        String regexDateAnglaise3 = "^(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])-[0-9]{4}";
+        Pattern p = Pattern.compile(regexDateFrancaise);
+        Pattern p2 = Pattern.compile(regexDateFrancaise2);
+                
+        Matcher m = p.matcher(date1);
+        Matcher m2 = p.matcher(date);
+        if(!m.matches()) {
+            fail();
+        }
+        if(m2.matches()) {
+            fail();
+        }
+        if(!p2.matcher(date2).matches()) {
+            fail();
+        }
+        if(!Pattern.matches(regexDateAnglaise3, date3)) {
+            fail();
+        }
+      }
 }
